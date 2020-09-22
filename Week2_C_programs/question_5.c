8362dc7af0471574a34c2f2756304deebf98e24c
Neal Kaul <71339849+yourFbiAgentBob@users.noreply.github.com>
2020-09-22 20:08:17 +0530

    Create question_5.c
    question 5 -Write a C/Java program to print the prime numbers between given two integers
    (inclusive). Accept these two integers from the user.

---
+++ Week2_C_programs/question_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int checkPrime(int n) {
+    int j, flag = 1;
+    for (j = 2; j <= n / 2; ++j) {
+        if (n % j == 0) {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main() {
+    int n1, n2, flag,i ;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+    printf("Prime numbers between %d and %d are: ", n1, n2);
+    for (i = n1 + 1; i < n2; ++i) {
+
+        flag = checkPrime(i);
+
+        if (flag == 1)
+            printf("%d ", i);
+    }
+    return 0;
+}
+
+
